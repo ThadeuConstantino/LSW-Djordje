c514b7e6660a04eb825867bd312355e6b59d43c1
thadeusantos <98712729+thadeusantos@users.noreply.github.com>
2022-01-31 15:29:57 -0300

    Camera Follow
    Player and Camera Follow

---
+++ LSW/Assets/Scripts/Heroe.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+namespace LSW.Heroe
+{
+    public class Heroe : MonoBehaviour
+    {
+        private Rigidbody2D rb2D;
+        private Animator anim;
+
+        public bool isDead;
+        public int health;
+
+        [SerializeField]
+        private int speed;
+        [SerializeField]
+        private float jumpforce;
+        [SerializeField]
+        private Transform groundCheck;
+
+        private bool grounded;
+        private bool jumping;
+
+        private bool facingRight;
+
+        private int maxJump;
+        public int totalJump;
+
+        public AudioClip fxJump;
+
+        void Start()
+        {
+            isDead = false;
+            maxJump = 0;
+            grounded = true;
+            facingRight = true;
+            
+            rb2D = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>(); 
+        }
+
+        private void Update()
+        {
+            if (isDead)
+                return;
+
+            grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+            if (Input.GetButtonDown("Jump") && maxJump > 0)
+            {
+               
+                grounded = false;
+                jumping = true;
+            }
+
+            if (grounded)
+            {
+                maxJump = totalJump;
+            }
+
+            SetAnimations();
+        }
+
+        private void FixedUpdate()
+        {
+            if (isDead)
+                return;
+
+            float move = Input.GetAxis("Horizontal");
+            rb2D.velocity = new Vector2(move * speed, rb2D.velocity.y);
+
+            if ((move < 0f && facingRight) || (move > 0f && !facingRight))
+            {
+                facingRight = !facingRight;
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+
+            if (jumping)
+            {
+                maxJump--;
+                jumping = false;
+
+                rb2D.velocity = new Vector2(rb2D.velocity.x, 0f);
+                rb2D.AddForce(new Vector2(0f, jumpforce));
+            }
+        }
+
+        private void SetAnimations()
+        {
+            anim.SetBool("Walk", (rb2D.velocity.x != 0f));
+            anim.SetFloat("VelY", rb2D.velocity.y);
+            anim.SetBool("JumpFall", !grounded);
+        }
+    }
+}
---
+++ LSW/Assets/Scripts/Utils/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSW.Utils
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        private Vector2 velocity;
+        private Transform heroe;
+
+        public float smoothTimeX;
+        public float smoothTimeY;
+
+        private void Start()
+        {
+            heroe = GameObject.Find("Heroe").GetComponent<Transform>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (heroe == null)
+                return;
+
+            float posX = Mathf.SmoothDamp(transform.position.x, heroe.position.x, ref velocity.x, smoothTimeX);
+            float posY = Mathf.SmoothDamp(transform.position.y, heroe.position.y, ref velocity.y, smoothTimeY);
+
+            transform.position = new Vector3(posX, posY, transform.position.z);
+        }
+    }
+}
