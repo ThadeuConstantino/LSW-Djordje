10e6e675ab1633f29d44bc38649eb9d8f598d035
thadeusantos <98712729+thadeusantos@users.noreply.github.com>
2022-01-31 18:49:01 -0300

    Interactive Objects
    interactive objects and free particles from the JMO Assets asset store

---
+++ LSW/Assets/Scripts/ScenaryObj/BreakFloor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace LSW.ScenaryObj
+{
+    public class BreakFloor : MonoBehaviour
+    {
+        public float time;
+        public GameObject particleBreak;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x,
+                                                            gameObject.transform.position.y - .025f,
+                                                            gameObject.transform.position.z);
+                StartCoroutine(DestroyFloor(other));
+            }
+        }
+
+        IEnumerator DestroyFloor(Collider2D other)
+        {
+            yield return new WaitForSeconds(time);
+
+            if (other)
+            {
+                Instantiate(particleBreak, gameObject.transform.position, gameObject.transform.rotation);
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ LSW/Assets/Scripts/ScenaryObj/VerticalPlataform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace LSW.ScenaryObj
+{
+    public class VerticalPlataform : MonoBehaviour
+    {
+        public float TopY;
+        public float DownY;
+        public float speed;
+
+        void Start()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+            float posAuxY = gameObject.transform.position.y + speed;
+
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x,
+                                                        posAuxY,
+                                                        gameObject.transform.position.z
+                                                        );
+
+            if (posAuxY > TopY || posAuxY < DownY)
+            {
+                speed *= -1;
+            }
+        }
+    }
+}
