b198749588e0fef0515e9312d1a592c49f11d319
thadeusantos <98712729+thadeusantos@users.noreply.github.com>
2022-02-01 21:43:26 -0300

    Guide for the game - Bone
    Guide to give warnings to the character during the level

---
+++ LSW/Assets/Scripts/ScenaryObj/WarningGuide.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LSW.ScenaryObj
+{
+    public class WarningGuide : MonoBehaviour
+    {
+        public GameObject _popUp;
+
+        private void Awake()
+        {
+            _popUp.SetActive(false);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.tag == "Player")
+            {
+                _popUp.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.tag == "Player")
+            {
+                _popUp.SetActive(false);
+            }
+        }
+    }
+}
